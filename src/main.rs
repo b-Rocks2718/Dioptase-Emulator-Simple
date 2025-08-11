8f3affe0e5d41ae68377fe3c6ec234d55c1d7b4f
Brooks Bryant <brooks.bryant@icloud.com>
2025-08-10 22:45:23 -0500

    input file parsing

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,14 @@ use emulator::Emulator;
 fn main() {
   let args = env::args().collect::<Vec<_>>();
 
-  if args.len() > 2 {
-    println!("Usage: bemu file.bin");
-    process::exit(64);
-  } else if args.len() == 2 {
+  if args.len() == 2 {
     // file to run is passed as a command line argument
     let mut cpu = Emulator::new(args[1].clone());
-    let result = cpu.run();
-    println!(""); // print a newline
-    process::exit(result as i32);
+    //let result = cpu.run();
+    //println!(""); // print a newline
+    //process::exit(result as i32);
   } else {
-    println!("Usage: bemu file.bin");
+    println!("Usage: cargo run -- file.hex");
     process::exit(64);
   }
 }
