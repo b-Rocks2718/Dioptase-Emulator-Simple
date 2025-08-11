ea39f6dff219a841b8623c3a15f1f9858c9b25e7
Brooks Bryant <brooks.bryant@icloud.com>
2025-08-10 22:18:31 -0500

    started emulator

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple Dioptase Emulator
+
+This emulator is for the user-mode subset of the [Dioptase ISA](https://github.com/b-Rocks2718/Dioptase/blob/main/docs/ISA.md)  
+
+It assumes all addresses have been mapped by the TLB (a segfault will never happen)
+
+## Usage
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::process;
+
+pub mod emulator;
+pub mod tests;
+
+use emulator::Emulator;
+
 fn main() {
-    println!("Hello, world!");
+  let args = env::args().collect::<Vec<_>>();
+
+  if args.len() > 2 {
+    println!("Usage: bemu file.bin");
+    process::exit(64);
+  } else if args.len() == 2 {
+    // file to run is passed as a command line argument
+    let mut cpu = Emulator::new(args[1].clone());
+    let result = cpu.run();
+    println!(""); // print a newline
+    process::exit(result as i32);
+  } else {
+    println!("Usage: bemu file.bin");
+    process::exit(64);
+  }
 }
