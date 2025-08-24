6a035bd4650b9b2cdbb6a4f58b82d9a6110fc7de
Brooks Bryant <brooks.bryant@icloud.com>
2025-08-24 13:28:37 -0500

    better mem test

--- tests/asm/mem_wr.s
+++ tests/asm/mem_wr.s
@@ -1,29 +1,18 @@
+.fill 0x44444444
+.fill 0x55555555
+.fill 0x66666666
 DATA_1: .fill 0xFFFFFFFF
+.fill 0x11111111
 
 _start:
   lw   r13, [DATA_1]
-  nop
-  nop
-  nop
   lw   r10, [DATA_2]
-  nop
-  nop
-  nop
   add  r13, r10, r13
-  nop
-  nop
-  nop
-  nop
   sw   r13, [DATA_2]
-  nop
-  nop
-  nop
-  lw   r3,  [DATA_2]
-  nop
-  nop
-  nop
-  nop
+  lw   r3,  [r0, DATA_2]
   sys  EXIT     # should return 0x25 = 37
 
-.space 277
+.space 128
+.fill 0x22222222
 DATA_2: .fill 38
+.fill 0x33333333
